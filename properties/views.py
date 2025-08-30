d3447760d5749d6f2f5a6ebc9bfcc598c4406dbc
Menu Temesgen <menutemesgen@gmail.com>
2025-08-30 17:32:56 +0300

    Implement caching for property list view and refactor data retrieval into a utility function

--- properties/views.py
+++ properties/views.py
@@ -4,9 +4,16 @@ from django.views.decorators.cache import cache_page
 from .models import Property
 
 
-@cache_page(60 * 15)  # cache for 15 minutes
+# @cache_page(60 * 15)  # cache for 15 minutes
+# def property_list(request):
+#     properties = Property.objects.all().values(
+#         "id", "title", "description", "price", "location", "created_at"
+#     )
+#     return JsonResponse({"data": list(properties)}, safe=False)
+
+from django.http import JsonResponse
+from .utils import get_all_properties
+
 def property_list(request):
-    properties = Property.objects.all().values(
-        "id", "title", "description", "price", "location", "created_at"
-    )
-    return JsonResponse({"data": list(properties)}, safe=False)
+    properties = get_all_properties()
+    return JsonResponse({"data": properties}, safe=False)
