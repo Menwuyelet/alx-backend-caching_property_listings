d73a8936b5a1f8048f5cc01410296139a0084c2b
Menu Temesgen <menutemesgen@gmail.com>
2025-08-30 17:29:41 +0300

    Refactor property_list view to improve readability and return data in a structured format

--- properties/views.py
+++ properties/views.py
@@ -3,7 +3,10 @@ from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
+
 @cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
-    properties = Property.objects.all().values("id", "title", "description", "price", "location", "created_at")
-    return JsonResponse(list(properties), safe=False)
+    properties = Property.objects.all().values(
+        "id", "title", "description", "price", "location", "created_at"
+    )
+    return JsonResponse({"data": list(properties)}, safe=False)
